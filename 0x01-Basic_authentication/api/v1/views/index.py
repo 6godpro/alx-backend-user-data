591d47a880039ca09ecaeb6e17f1378ba3e50fec
6godpro <erickson5115@gmail.com>
2023-11-18 07:43:33 +0100

    disable strict slashes

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized')
+@app_views.route('/unauthorized', strict_slashes=False)
 def unauthorized():
     """Test unauthorized error handler."""
     abort(401)
