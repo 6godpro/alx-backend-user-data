4f3f6979ef9795c285a2f5323029391655829595
6godpro <erickson5115@gmail.com>
2023-11-18 05:03:16 +0100

    solution to task 0 and 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 """Filters a log line."""
+import logging
+import re
+
+from datetime import datetime
 from typing import List
 
 
@@ -19,15 +23,32 @@ def filter_datum(fields: List,
                         character is separating all fields
                         in the log line (message).
     """
-    _dict = {}
-    data = message.split(separator)
-    for d in data:
-        if len(d):
-            d = d.split('=')
-            _dict[d[0]] = d[1]
     for field in fields:
-        _dict[field] = redaction
-    string = ''
-    for k, v in _dict.items():
-        string += k + '=' + v + separator
-    return string
+        pattern: str = r'{}=([^{}]+)'.format(field, separator)
+        print(pattern)
+        replacement: str = '{}={}'.format(field, redaction)
+        message = re.sub(pattern, replacement, message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format the log record"""
+        filtered_message = filter_datum(self.fields, self.REDACTION,
+                                        record.msg, self.SEPARATOR)
+        asctime = datetime.now().strftime("%Y-%m-%d %H:%M:%S,%f")[:-3]
+        values = {
+            "name": record.name, "levelname": record.levelname,
+            "asctime": asctime, "message": filtered_message
+        }
+        return self.FORMAT % values
